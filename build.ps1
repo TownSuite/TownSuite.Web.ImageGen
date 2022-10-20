63c82742415ec0067501b243a18fb0b9c529fa54
Peter Gill <peter.gill@townsuite.com>
2022-10-20 17:05:23 -0230

    build and publish scripts

---
+++ build.ps1
@@ -0,0 +1,21 @@
+#!/usr/bin/env pwsh
+$ErrorActionPreference = "Stop"
+$CURRENTPATH=$pwd.Path
+
+# Must install powershell:  https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu?view=powershell-7.2
+
+$VERSION = cat TownSuite.Web.ImageGen/TownSuite.Web.ImageGen.csproj | grep "<Version>"  | sed 's/[^0-9.]*//g' 
+rm -rf build
+mkdir -p build
+$GITHASH="$(git rev-parse --short HEAD)"
+echo "$GITHASH" >> build/githash.txt
+
+Write-Host "Building townsuite/imagegen:latest" -ForegroundColor Green
+docker build -f "$CURRENTPATH/TownSuite.Web.ImageGen/Dockerfile" -t townsuite/imagegen$GITHASH -m 4GB .
+
+
+Write-Host "Building imagegen.tar" -ForegroundColor Green
+docker save townsuite/imagegen$GITHASH -o "$CURRENTPATH/build/imagegen.tar"
+docker rmi "townsuite/imagegen$GITHASH"
+Write-Host "Finished imagegen.tar" -ForegroundColor Green
+
---
+++ publish.ps1
@@ -0,0 +1,13 @@
+#!/usr/bin/env pwsh
+$ErrorActionPreference = "Stop"
+$CURRENTPATH=$pwd.Path
+
+# Must install powershell:  https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu?view=powershell-7.2
+# Must install google crane:  https://github.com/google/go-containerregistry/releases
+
+$GITHASH = cat build/githash.txt
+
+crane push "$CURRENTPATH/build/imagegen.tar" townsuite/imagegen:latest
+crane push "$CURRENTPATH/build/imagegen.tar" townsuite/imagegen:${GITHASH}
+
+
