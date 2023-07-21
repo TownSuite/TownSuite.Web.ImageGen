76a741eb7b2dbafc6714425fe0ce4fa963c59166
JoshuaPeddle <joshuapeddle128@gmail.com>
2023-07-21 10:47:20 -0230

    Use ImageFormat class to reduce duplicated image format detection code.

---
+++ TownSuite.Web.ImageGen/ImageFormat.cs
@@ -0,0 +1,43 @@
+﻿namespace TownSuite.Web.ImageGen
+{
+    public class ImageFormat
+    {
+        public enum Format
+        {
+            jpeg,
+            png,
+            gif,
+            bmp,
+            tiff,
+            webp,
+            svg,
+            heic,
+            avif
+        }
+        public static bool IsFormat(string formatStr, Format type)
+        {
+            if (formatStr.StartsWith("image/"))
+            {
+                formatStr = formatStr[6..];
+            }
+            if (formatStr.Contains("jpg")) formatStr = "jpeg";
+            if (formatStr == "heif") formatStr = "heic";
+            if (formatStr.Contains("svg")) formatStr = "svg";
+            return string.Equals(formatStr, type.ToString(), StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public static Format GetFormat(string formatStr)
+        {
+            if (formatStr.StartsWith("image/"))
+            {
+                formatStr = formatStr[6..];
+            }
+            if (formatStr.Contains("jpg")) formatStr = "jpeg";
+            if (formatStr == "heif") formatStr = "heic";
+            if (formatStr.Contains("svg")) formatStr = "svg";
+            return (Format)Enum.Parse(typeof(Format), formatStr, true);
+            
+        }
+
+    }
+}
