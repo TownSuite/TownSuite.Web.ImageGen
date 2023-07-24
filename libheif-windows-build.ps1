cd0c74376c0c809eab675cf795e713e26fb20b6b
JoshuaPeddle <joshuapeddle128@gmail.com>
2023-07-24 12:00:18 -0230

    Properly quote filepaths

--- libheif-windows-build.ps1
+++ libheif-windows-build.ps1
@@ -11,16 +11,16 @@ else {
     New-Item "libheif-build" -ItemType Directory
 }
 
-git clone -q --depth 1 https://github.com/microsoft/vcpkg libheif-build
+git clone -q --depth 1 "https://github.com/microsoft/vcpkg" libheif-build
 
 Invoke-Expression -Command ".\libheif-build\bootstrap-vcpkg.bat"
 
 .\libheif-build\vcpkg install aom:x64-windows libde265:x64-windows x265:x64-windows libheif:x64-windows
 
-Rename-Item -Path .\libheif-build\installed\x64-windows\bin\heif.dll -NewName "libheif.dll"
+Rename-Item -Path ".\libheif-build\installed\x64-windows\bin\heif.dll" -NewName "libheif.dll"
 
-Copy-Item -Path .\libheif-build\installed\x64-windows\bin\*.dll -Destination .\TownSuite.Web.ImageGen\bin\Debug\net6.0
+Copy-Item -Path ".\libheif-build\installed\x64-windows\bin\*.dll" -Destination ".\TownSuite.Web.ImageGen\bin\Debug\net6.0"
 
-Copy-Item -Path .\libheif-build\installed\x64-windows\bin\*.dll -Destination .\TownSuite.Web.ImageGen.Tests\bin\Debug\net6.0
+Copy-Item -Path ".\libheif-build\installed\x64-windows\bin\*.dll" -Destination ".\TownSuite.Web.ImageGen.Tests\bin\Debug\net6.0"
 
 Remove-Item "libheif-build" -Recurse -Force
