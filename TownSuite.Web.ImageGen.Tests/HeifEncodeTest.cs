683cb562ac9382d8a7ae38831fcc4cc3e38f31e6
JoshuaPeddle <joshuapeddle128@gmail.com>
2023-07-25 15:22:03 -0230

    Add more cases for encoder.
    Test a larger variety of images.

--- TownSuite.Web.ImageGen.Tests/HeifEncodeTest.cs
+++ TownSuite.Web.ImageGen.Tests/HeifEncodeTest.cs
@@ -8,23 +8,28 @@ namespace TownSuite.Web.ImageGen.Tests;
 [TestFixture]
 public class HeifEncodeTest
 {
-    [Test]
-    public async Task CanConvertSharpToHeif()
+    [TestCase("color-no-alpha.png",     800, 800),
+     TestCase("grayscale-no-alpha.png", 800, 800),
+     TestCase("grayscale-alpha.png",    980, 981),
+     TestCase("color-alpha.png",        961, 611),
+     TestCase("facility.jpg",           800, 365)]
+    public async Task CanConvertSharpToHeif(string imageName, int width, int height)
     {
-        var origImage = await Image.LoadAsync("assets/facility.jpg");
-        Assert.That(origImage.Height, Is.EqualTo(365));
-        Assert.That(origImage.Width, Is.EqualTo(800));
+        var origImage = await Image.LoadAsync($"assets/{imageName}");
+        Assert.That(origImage.Height, Is.EqualTo(height));
+        Assert.That(origImage.Width, Is.EqualTo(width));
 
         HeifImage image = HeifEncoder.ConvertSharpToHeif(origImage.CloneAs<Rgba32>());
 
-        Assert.That(image.Height, Is.EqualTo(365));
-        Assert.That(image.Width, Is.EqualTo(800));
+        Assert.That(image.Height, Is.EqualTo(height));
+        Assert.That(image.Width, Is.EqualTo(width));
     }
 
-    [TestCase("color-no-alpha.png", false, false),
-     TestCase("grayscale-no-alpha.png", true, false),
-     TestCase("grayscale-alpha.png", true, true),
-     TestCase("color-alpha.png", false, true)]
+    [TestCase("color-no-alpha.png",     false, false),
+     TestCase("grayscale-no-alpha.png", true,  false),
+     TestCase("grayscale-alpha.png",    true,  true),
+     TestCase("color-alpha.png",        false, true),
+     TestCase("facility.jpg",           false, false)]
     public async Task CanAnalyzeImage(string imageName, bool expectGrayscale, bool expectTransparency)
     {
         var origImage = await Image.LoadAsync($"assets/{imageName}");
