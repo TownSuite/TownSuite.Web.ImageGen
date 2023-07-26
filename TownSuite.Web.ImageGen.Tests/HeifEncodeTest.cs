867f0a454e4f89f9602402d01bd26d6c6c56cfe8
Joshua Peddle <Joshuapeddle128@gmail.com>
2023-07-26 08:21:33 -0230

    Add new tests to check that pixels arent changed after conversion

--- TownSuite.Web.ImageGen.Tests/HeifEncodeTest.cs
+++ TownSuite.Web.ImageGen.Tests/HeifEncodeTest.cs
@@ -2,6 +2,7 @@
 using LibHeifSharp;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using System.Runtime.InteropServices;
 
 namespace TownSuite.Web.ImageGen.Tests;
 
@@ -37,5 +38,57 @@ public class HeifEncodeTest
         (bool isGrayscale, bool hasTransparency) = HeifEncoder.AnalyzeImage(origImage.CloneAs<Rgba32>());
         Assert.That(isGrayscale, Is.EqualTo(expectGrayscale));
         Assert.That(hasTransparency, Is.EqualTo(expectTransparency));
+    }
+
+    [TestCase("color-no-alpha.png"),
+     TestCase("color-alpha.png")]
+    public async Task ColorPixelsArentModifed(string imageName)
+    {
+        Image<Rgba32> origImage = Image.Load($"assets/{imageName}").CloneAs<Rgba32>();
+
+        using var context = new HeifContext();
+        HeifImage heifImage = HeifEncoder.ConvertSharpToHeif(origImage);
+        LibHeifSharp.HeifEncoder encoder = context.GetEncoder(HeifCompressionFormat.Hevc);
+        encoder.SetLossyQuality(90); // Quality higher to avoid artifacts
+        context.EncodeImage(heifImage, encoder);
+
+        var origPixel = origImage[0, 0];
+        var origR = origPixel.R;
+        var origG = origPixel.G;
+        var origB = origPixel.B;
+
+        HeifPlaneData grayPlane = heifImage.GetPlane(HeifChannel.Interleaved);
+        IntPtr grayStartPtr = grayPlane.Scan0;
+
+        var r = Marshal.ReadByte(grayStartPtr);
+        var g = Marshal.ReadByte(grayStartPtr + 1);
+        var b = Marshal.ReadByte(grayStartPtr + 2);
+       
+        Assert.That(r, Is.InRange(origR - 1, origR + 1));
+        Assert.That(g, Is.InRange(origG - 1, origG + 1));
+        Assert.That(b, Is.InRange(origB - 1, origB + 1));
+    }
+
+    [TestCase("grayscale-no-alpha.png"),
+    TestCase("grayscale-alpha.png")]
+    public async Task GrayscalePixelsArentModifed(string imageName)
+    {
+        Image<Rgba32> origImage = Image.Load($"assets/{imageName}").CloneAs<Rgba32>();
+
+        using var context = new HeifContext();
+        HeifImage heifImage = HeifEncoder.ConvertSharpToHeif(origImage);
+        LibHeifSharp.HeifEncoder encoder = context.GetEncoder(HeifCompressionFormat.Hevc);
+        encoder.SetLossyQuality(90); // Quality higher to avoid artifacts
+        context.EncodeImage(heifImage, encoder);
+
+        var origPixel = origImage[0, 0];
+        var origR = origPixel.R;
+
+        HeifPlaneData grayPlane = heifImage.GetPlane(HeifChannel.Y);
+        IntPtr grayStartPtr = grayPlane.Scan0;
+
+        var r = Marshal.ReadByte(grayStartPtr);
+
+        Assert.That(r, Is.InRange(origR - 1, origR + 1));
     }
 }
