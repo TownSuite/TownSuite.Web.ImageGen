126f7a67addd683341d2ffe23189695d4dfa1feb
JoshuaPeddle <joshuapeddle128@gmail.com>
2023-07-27 11:31:02 -0230

    Add new cases for ImageFormatTests

--- TownSuite.Web.ImageGen.Tests/ImageFormatTest.cs
+++ TownSuite.Web.ImageGen.Tests/ImageFormatTest.cs
@@ -4,17 +4,17 @@ namespace TownSuite.Web.ImageGen.Tests;
 [TestFixture]
 public class ImageFormatTest
 {
-    [TestCase("png" ,ImageFormat.Format.png),
+    [TestCase("png",       ImageFormat.Format.png),
      TestCase("image/jpg", ImageFormat.Format.jpeg),
-     TestCase("svg+xml", ImageFormat.Format.svg),
-     TestCase("   heif ", ImageFormat.Format.heic)]
+     TestCase("svg+xml",   ImageFormat.Format.svg),
+     TestCase("   heif ",  ImageFormat.Format.heic)]
     public void ReturnsExpectedFormat(string given, ImageFormat.Format expected)
     {
         ImageFormat.Format returnedFormat = ImageFormat.GetFormat(given);
         Assert.That(returnedFormat, Is.EqualTo(expected), $"Expected {expected} but got {returnedFormat}");
     }
 
-    private static readonly string[] cases = new string[] {" ", "", "abxz", "image/"};
+    private static readonly string[] cases = new string[] { " ", "", "abxz", "image/", "123456" };
     [TestCaseSource(nameof(cases))]
     public void ReturnsPngOnInvalidType(string given)
     {
