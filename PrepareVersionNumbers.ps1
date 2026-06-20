c1ab4719754b1ca19d2a115f14fc3bae127c33ac
Peter Gill <peter.gill@townsuite.com>
2026-06-20 18:08:48 -0230

    Add PowerShell script for version number updates in build process

---
+++ PrepareVersionNumbers.ps1
@@ -0,0 +1,15 @@
+#!/usr/bin/env pwsh
+$ErrorActionPreference = "Stop"
+$CURRENTPATH=$pwd.Path
+
+
+function RunVerionUpdater($loc, $path){
+	If ($IsWindows) {
+	    & "./tools/AssemblyInfoUtil.exe" -inc:$loc "$path"
+	}
+	else {
+		assemblyinfoutil -inc:$loc "$path"
+	}
+}
+
+RunVerionUpdater 3 "$CURRENTPATH/Directory.Build.props"
